326bcc6caa123483a980fb58e2af612373377165
Constantine Zaytsev <bassneck@gmail.com>
2015-12-01 22:27:39 +1300

    Extract exceptions into separate condition branches

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -8,42 +8,44 @@ class GildedRose
     @items.each do |item|
       next if item.name == "Sulfuras, Hand of Ragnaros"
 
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          item.quality = item.quality - 1
-        end
-      else
+      if item.name == "Aged Brie"
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+        end
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality < 50
+          item.quality = item.quality + 1
+
+          if item.sell_in < 11
+            if item.quality < 50
+              item.quality = item.quality + 1
             end
           end
+
+          if item.sell_in < 6
+            if item.quality < 50
+              item.quality = item.quality + 1
+            end
+          end
+        end
+      else
+        if item.quality > 0
+          item.quality = item.quality - 1
         end
       end
 
       item.sell_in = item.sell_in - 1
 
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              item.quality = item.quality - 1
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
+        if item.name == "Aged Brie"
           if item.quality < 50
             item.quality = item.quality + 1
+          end
+        elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+          item.quality = item.quality - item.quality
+        else
+          if item.quality > 0
+            item.quality = item.quality - 1
           end
         end
       end
